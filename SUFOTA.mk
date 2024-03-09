f1861b80ef1b3b81e0e9b9c0a8f8b3f899b17e21
RanaAnwar1 <104921409+RanaAnwar1@users.noreply.github.com>
2024-03-09 13:50:01 +0200

    Add files via upload

---
+++ SUFOTA.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# SUFOTA
+#
+################################################################################
+
+SUFOTA_VERSION = 1.0
+SUFOTA_SITE = https://github.com/RanaAnwar1/SUFOTA
+SUFOTA_SITE_METHOD = git
+
+define SUFOTA_BUILD_CMDS
+    $(CC) $(CPPFLAGS) $(CFLAGS) -o $(@D)/main $(@D)/$(SUFOTA_SOURCE)
+endef
+
+define SUFOTA_INSTALL_TARGET_CMDS
+    $(INSTALL) -D -m 755 $(@D)/main $(TARGET_DIR)/usr/bin/main
+endef
+
+$(eval $(generic-package))
